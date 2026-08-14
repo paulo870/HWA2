ab920a806c64ab16617cd925d1d398806b84bf2f
paulo870 <paulo@poliglotaangola.com>
2026-08-14 16:42:30 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -103,17 +103,17 @@ function loadStudentUnit(unitNumber) {
     let pageNumbers = [];
     switch(unitNumber) {
         case "1": pageNumbers = [9,10,11,12,13,14,15,16,17,18]; break;
-        case "2": pageNumbers = [12,13,14,15,16,17]; break;
-        case "3": pageNumbers = [18,19,20,21,22,23,24,25]; break;
-        case "4": pageNumbers = [26,27,28,29,30,31]; break;
-        case "5": pageNumbers = [32,33,34,35,36,37]; break;
-        case "6": pageNumbers = [38,39,40,41,42,43,44,45]; break;
-        case "7": pageNumbers = [46,47,48,49,50,51]; break;
-        case "8": pageNumbers = [52,53,54,55,56,57]; break;
-        case "9": pageNumbers = [58,59,60,61,62,63,64,65]; break;
-        case "10": pageNumbers = [66,67,68,69,70,71]; break;
-        case "11": pageNumbers = [72,73,74,75,76,77]; break;
-        case "12": pageNumbers = [78,79,80,81,82,83,84,85]; break;
+        case "2": pageNumbers = [19,20,21,22,23,24,25,26,27,28]; break;
+        case "3": pageNumbers = [29,30,31,32,33,34,35,36,37,38]; break;
+        case "4": pageNumbers = [39,40,41,42,43,44,45,46,47,48]; break;
+        case "5": pageNumbers = [49,50,51,52,53,54,55,56,57,58]; break;
+        case "6": pageNumbers = [59,60,61,62,63,64,65,66,67,68]; break;
+        case "7": pageNumbers = [69,70,71,72,73,74,75,76,77,78]; break;
+        case "8": pageNumbers = [79,80,81,82,83,84,85,86,87,88]; break;
+        case "9": pageNumbers = [89,90,91,92,93,94,95,96,97,98]; break;
+        case "10": pageNumbers = [99,100,101,102,103,104,105,106,107,108]; break;
+        case "11": pageNumbers = [109,110,111,112,113,114,115,116,117,118]; break;
+        case "12": pageNumbers = [119,120,121,122,123,124,125,126,127,128]; break;
         default: pageNumbers = []; break;
     }
 
